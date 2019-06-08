d4b4bda44b8c2c6c98884ef37ffe18f3d34eee87
k-waragai <kazuxtuna@gmail.com>
2019-06-09 00:53:47 +0900

    入力待ち状態を作成

--- provisioning/get_lane_id.rb
+++ provisioning/get_lane_id.rb
@@ -1,5 +1,6 @@
-require '../lib/lane_id'
-require '../lib/board_id'
+require './lib/lane_id'
+require './lib/board_id'
+
 
 body = BoardId.get
 body.each{|b| puts b}
@@ -8,11 +9,10 @@ puts "------------------------------"
 puts "対象のボードのIDを入力してください。"
 puts "対象ボードIDは上記で表示されているIDのいずれかをコピペし入力してください。"
 
-p "board_id: "
+print "BoardID >>  "
 board_id = gets.chomp!
 
 puts "Success."
-
 puts "------------------------------"
 
 body = LaneId.get(board_id: board_id)
